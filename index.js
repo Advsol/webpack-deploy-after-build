8a6db5acfcf3c183171e83da14d57997508c4bef
pmb5 <pmb5@users.noreply.github.com>
2018-02-06 12:50:21 +0000

    Allow multiple destination folders + suffix of the PublicPath to the destination

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 'use strict';
-const ncp = require('ncp').ncp;
+//const ncp = require('ncp').ncp;
+const fs = require('fs-extra');
+
 const del = require('del');
+const path = require('path');
+const clc = require('cli-color');
+
+// remove first character if it's the asp.net path tilde
+function cleanPublicPath(publicPath) {
+    if (publicPath.startsWith("~")) {
+        return publicPath.substring(1);
+    }
+    return publicPath;
+}
 
 function WebPackDeployAfterBuild(options) {
     var defaultOptions = {
@@ -16,17 +28,24 @@ WebPackDeployAfterBuild.prototype.apply = function(compiler) {
     compiler.plugin("done", (stats) => {
         console.log(Object.keys(stats.compilation.assets));
         console.log("\nExecuting Deploy on done...");
-        if (options.clearDestDir) {
-            del.sync([`${options.to}/*`], {force: true});
-        }
-        ncp(options.from, options.to, function(err) {
-            if (err) {
-                console.error("Err in ncp check the paths");
-            }
-            console.log(`Finished deploying ${options.from} to ${options.to}`);
-        });
-    });
 
+        for (var i = 0, len = options.to.length; i < len; i++) {
+
+            var to = options.to[i];
+            if (options.suffixPublicPath)
+                to = path.join(options.to[i], cleanPublicPath(compiler.options.output.publicPath));
+
+            
+            if (options.clearDestDir) {
+                del.sync([`${options.to[i]}/*`], { force: true });
+            }
+
+            fs.copy(options.from, to, { overwrite :true})
+                .then(() => console.log(`Finished deploying to: ${clc.cyan(to)}`))
+                .catch(err => console.error(err));
+        }
+
+    });
 };
 
 module.exports = WebPackDeployAfterBuild;
